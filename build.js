02274839aa16cef45e33fa03ce5fb93767d7ea90
IshaanOhri <ishaan99ohri@gmail.com>
2020-07-31 00:43:21 +0530

    Auto update of packages added

--- build.js
+++ build.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
-import logger from './src/logger/config';
-
 const childProcess = require('child_process');
 const fs = require('fs');
 
-logger.info('Building application...');
+console.log('Building application...');
 const { env } = process;
 env.NODE_ENV = 'production';
 childProcess.execSync('gulp', {
@@ -12,16 +10,18 @@ childProcess.execSync('gulp', {
 	stdio: [0, 1, 2]
 });
 
-logger.debug('Correcting package.json...');
+console.log('Correcting package.json...');
+childProcess.execSync('npx npm-check-updates -u');
 const pkg = JSON.parse(fs.readFileSync('./package.json').toString());
 if (pkg.scripts && pkg.scripts.start)
 	pkg.scripts.start = `NODE_ENV=production ${pkg.scripts.start
 		.replace('ts-node', 'node')
 		.replace('.ts', '.js')}`;
 if (pkg.devDependencies) delete pkg.devDependencies;
+pkg.main = `${pkg.main.replace('.ts', '.js')}`;
 fs.writeFileSync('./bin/package.json', JSON.stringify(pkg, null, '\t'));
 
-logger.debug('Installing production dependencies...');
+console.log('Installing production dependencies...');
 childProcess.execSync('npm install --only=production', {
 	cwd: './bin',
 	env,
