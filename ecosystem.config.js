5cf3c04cb2c48b0101098434d05595abfb8bdc09
IshaanOhri <ishaan99ohri@gmail.com>
2020-07-31 13:05:05 +0530

    Update ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	apps: [
+		{
+			name: 'bulletproof-nodejs-template',
+			script: './bin/index.js',
+			instances: 'max',
+			exec_mode: 'cluster',
+			env: {
+				NODE_ENV: 'production',
+				PORT: 3000,
+				HOST: '',
+				NEWRELIC_KEY: ''
+			}
+		}
+	]
+};
