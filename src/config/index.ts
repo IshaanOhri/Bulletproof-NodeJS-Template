05d7b01b64f8cf3cae23b947232e63adc2736df3
IshaanOhri <ishaan99ohri@gmail.com>
2020-12-09 14:56:23 +0530

    added index.ts to config folder

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,9 @@
+/*
+ * @Author: Ishaan Ohri
+ * @Date: 2020-12-09 14:55:52
+ * @Last Modified by: Ishaan Ohri
+ * @Last Modified time: 2020-12-09 14:55:52
+ * @Description: Indexing for entire config folder
+ */
 export * from './response';
 export * from './env';
